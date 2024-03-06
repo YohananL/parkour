d3ee19d5b230385418d38b429d11701d6f69e75d
yohananl <dartdartpart@gmail.com>
2024-03-05 22:25:01 -0800

    Change max height and add extra check if value is nil

--- parkour/client/client.lua
+++ parkour/client/client.lua
@@ -55,7 +55,7 @@ local HeightLevels = {
     Low    = -0.2,
     Medium = 0.4,
     High   = 1.8,
-    Max    = 2.4,
+    Max    = 2.5,
 }
 
 local ForwardDistance = 1.0
@@ -70,13 +70,13 @@ function GetEntInFrontOfPlayer(Ped)
     local heightIndex = HeightLevels.Min
     local firstHit = nil
     local lastHit = nil
-    while heightIndex <= HeightLevels.Max do
+    while heightIndex <= HeightLevels.Max + 0.01 do
         local CoA = GetEntityCoords(Ped, true)
         local CoB = GetOffsetFromEntityInWorldCoords(Ped, 0.0, ForwardDistance, heightIndex)
         local RayHandle = StartExpensiveSynchronousShapeTestLosProbe(CoA.x, CoA.y, CoB.z,
             CoB.x, CoB.y, CoB.z, -1, Ped, 0) -- -1 = Everything
 
-        local _, hit, endCoords, surfaceNormal, materialHash, entityHit =
+        _, hit, _, _, _, _ =
             GetShapeTestResultIncludingMaterial(RayHandle)
 
         if hit == 1 then
@@ -85,6 +85,10 @@ function GetEntInFrontOfPlayer(Ped)
             else
                 lastHit = heightIndex
             end
+        else
+            if firstHit and lastHit then
+                break
+            end
         end
 
         heightIndex = heightIndex + 0.1
@@ -92,7 +96,7 @@ function GetEntInFrontOfPlayer(Ped)
         Wait(1)
     end
 
-    return firstHit, lastHit, endCoords
+    return firstHit, lastHit
 
     -- while true do
     --     local heightIndex = 1.7
@@ -254,11 +258,17 @@ end
 
 function reverseVault(playerPed, heightLevel)
     TaskClimb(playerPed, false)
-    print('difference: ' .. tostring(HeightLevels.High - heightLevel))
-    if HeightLevels.High - heightLevel <= 0.5 then
+    local difference = HeightLevels.High - heightLevel
+    print('difference: ' .. tostring(difference))
+    if difference <= 0.5 then
+        print('here 1')
         Wait(1200)
-    else
+    elseif difference <= 0.8 then
+        print('here 2')
         Wait(700)
+    else
+        print('here 3')
+        Wait(450)
     end
 
     ClearPedTasksImmediately(playerPed)
@@ -339,13 +349,17 @@ function tictakRight(playerPed)
     Wait(animTime * 450)
 end
 
-function ledgeJumpUp(playerPed)
+function ledgeJumpUp(playerPed, heightLevel)
     local x, y, z = table.unpack(GetEntityCoords(playerPed))
 
     local animTime = doAnimation(playerPed, ParkourAnimations.ledge.ledgeJumpUp)
     Wait(animTime * 1000)
 
-    SetEntityCoords(playerPed, x, y, z + 1.43, true, false, false, false)
+    local offset = 1.435 - ((HeightLevels.Max - heightLevel) * 1.5)
+    print('offset: ' .. tostring(offset))
+
+    SetEntityCoords(playerPed, x, y, z + offset, true, false, false, false)
+
     SetEntityHasGravity(playerPed, false)
 
     while true do
@@ -379,7 +393,6 @@ function ledgeJumpUp(playerPed)
         Wait(0)
     end
 
-
     SetEntityHasGravity(playerPed, true)
 end
 
@@ -399,30 +412,32 @@ RegisterCommand('+parkour', function()
 
     local playerPed = PlayerPedId()
 
-    -- Load all sit animations
+    -- Load all parkour animations
     loadParkourAnimations()
 
     -- Get if there's object in front of the ped
-    local firstHit, lastHit, endCoords = GetEntInFrontOfPlayer(playerPed)
+    local firstHit, lastHit = GetEntInFrontOfPlayer(playerPed)
 
     print('firstHit: ' .. tostring(firstHit))
     print('lastHit: ' .. tostring(lastHit))
 
     if firstHit ~= nil then
-        if firstHit > HeightLevels.Min and firstHit <= HeightLevels.Low then
+        if firstHit > 0.0 and firstHit <= HeightLevels.Medium then
             slide(playerPed)
-        elseif lastHit <= HeightLevels.Medium then
-            kashVault(playerPed)
-        elseif lastHit <= HeightLevels.High then
-            reverseVault(playerPed, lastHit)
-        elseif lastHit < HeightLevels.Max then
-            ledgeJumpUp(playerPed)
-        else
-            wallFlip(playerPed)
+        elseif lastHit ~= null then
+            if lastHit <= HeightLevels.Medium then
+                kashVault(playerPed)
+            elseif lastHit <= HeightLevels.High then
+                reverseVault(playerPed, lastHit)
+            elseif lastHit < HeightLevels.Max then
+                ledgeJumpUp(playerPed, lastHit)
+            else
+                wallFlip(playerPed)
+            end
         end
     end
 
-    -- Unload all sit animations
+    -- Unload all parkour animations
     unloadParkourAnimations()
 
     isDoingParkour = false
